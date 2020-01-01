3cb8c0d1dbe28aa930e3be49567806004371e149
Flare Porterfield <flare@baanmhee.com>
2020-01-01 11:36:08 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Nattapong Panprome on 1/1/20.
+//  Copyright © 2020 Baanmhee Entertainment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
